10951920b70a72a55cf0bdb0df565d437a4cec64
Gabriel Pissaia <89611752+GabrielPiss@users.noreply.github.com>
2023-12-04 19:55:27 -0300

    Commit primeirode user CRUD finalizado

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -15,13 +15,52 @@ module.exports = {
 
     async store(req, res) {
 
+    const { name, password, email } = req.body;
+
+    const user = await User.create({ name, password, email });
+
+    return res.status(200).send({
+        status: 1,
+        menssage: 'usuario cadastrado com sucesso!',
+        user
+    });
+
     },
 
     async update(req, res) {
 
+    const { name, password, email } = req.body;
+    const { user_id } = req.params;
+
+    await User.update({
+        name, password, email
+    }, {
+        where: {
+            id: user_id
+        }
+    });
+
+    return res.status(200).send({
+        status: 1,
+        menssage: "Usuario atualizado com sucesso!",
+    });
+
     },
 
     async delete(req, res) {
+
+        const { user_id } = req.params;
+
+        await User.destroy({
+            where:{
+                id: user_id
+            } 
+    });
+        
+    return res.status(200).send({
+        status: 1,
+        menssage: "Usuario deletado com sucesso!",
+    });
 
     },
 
