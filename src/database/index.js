f26d6045b02f026a16ae648a0118a8c471edf4cb
Gabriel Pissaia <89611752+GabrielPiss@users.noreply.github.com>
2023-12-04 17:02:22 -0300

    Commit migration de user criado

--- src/database/index.js
+++ src/database/index.js
@@ -4,11 +4,4 @@ const dbConfig = require('../config/database.js');
 
 const connection = new Sequelize(dbConfig);
 
-try {
-    connection.authenticate();
-        console.log('conectado com sucesso');
-    } catch (erro) {
-        console.error('erro ao conectar', error);    
-    }
-
 module.exports = connection;
