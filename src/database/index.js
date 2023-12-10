7bf19337c23538ddd74582cf081d1ca9d3860a0f
Gabriel Pissaia <89611752+GabrielPiss@users.noreply.github.com>
2023-12-10 17:18:28 -0300

    Commit finalizando o projeto, adicionando 5 cadastros de cada CRUD

--- src/database/index.js
+++ src/database/index.js
@@ -25,14 +25,29 @@ connection.authenticate()
     return connection.sync({ force: true })
       .then(() => {
         return User.bulkCreate([
-          { name: 'Gabriel', password: bcrypt.hashSync('000', 10), email: 'Gabriel@example.com' },
-          { name: 'Camilla', password: bcrypt.hashSync('000', 10), email: 'Camilla@example.com' },
-          { name: 'Monique', password: bcrypt.hashSync('000', 10), email: 'Monique@example.com' }
+          { name: 'Gabriel', password: bcrypt.hashSync('000', 10), email: 'Gabriel@gmail.com' },
+          { name: 'Camilla', password: bcrypt.hashSync('000', 10), email: 'Camilla@gmail.com' },
+          { name: 'Monique', password: bcrypt.hashSync('000', 10), email: 'Monique@gmail.com' },
+          { name: 'Carol', password: bcrypt.hashSync('000', 10), email: 'Carol@gmail.com' },
+          { name: 'Alexandre', password: bcrypt.hashSync('000', 10), email: 'Alexandre@gmail.com' },
+        ]);
+      })
+      .then(() => {
+        return Address.bulkCreate([
+          { street: 'Rua 1', number: '123', district: 'Centro', city: 'Cidade A', user_id: 1 },
+          { street: 'Rua 2', number: '456', district: 'Bairro B', city: 'Cidade B', user_id: 2 },
+          { street: 'Rua 3', number: '789', district: 'Bairro C', city: 'Cidade C', user_id: 3 },
+          { street: 'Rua 4', number: '101', district: 'Bairro D', city: 'Cidade D', user_id: 4 },
+          { street: 'Rua 5', number: '131', district: 'Bairro E', city: 'Cidade E', user_id: 5 },
         ]);
       })
       .then(() => {
         return Admin.bulkCreate([
-          { name: 'Chefe', password: bcrypt.hashSync('000', 10), email: 'Chefe@example.com' }
+          { name: 'Chefe', password: bcrypt.hashSync('000', 10), email: 'Chefe@gmail.com' },
+          { name: 'Captao', password: bcrypt.hashSync('000', 10), email: 'Capitao@gmail.com' },
+          { name: 'Superior', password: bcrypt.hashSync('000', 10), email: 'Superior@gmail.com' },
+          { name: 'Presidente', password: bcrypt.hashSync('000', 10), email: 'Presidente@gmail.com' },
+          { name: 'Administrador', password: bcrypt.hashSync('000', 10), email: 'Administrador@gmail.com' },
         ]);
       });
   })
